bab49443beeaf4620789b6302fedc55e89681502
juanfariasdev <juanpablo.profissional.a.f@gmail.com>
2022-08-17 22:00:41 -0300

    Init

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+let isover= false;
+document.querySelector("#no").addEventListener("mouseover",(function(){
+    Move()
+}), false)
+
+document.querySelector("#noWithBorder").addEventListener("mouseover",(function(){
+    Move()
+}), false)
+
+document.
+querySelector("#no").addEventListener("mouseout",(function(){
+    Move()
+}), false)
+
+function Move(){
+    let e = document.querySelector('#noWithBorder');
+
+    e.style.position = "absolute";
+
+    e.style.right = Math.floor(Math.random() * 100) + "%"
+    e.style.bottom = Math.floor(Math.random() * 100) + "%"
+
+
+}
